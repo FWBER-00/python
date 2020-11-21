ae43b6919ffb79275694863ff91375cabf5eaf00
FWBER-00 <74819519+FWBER-00@users.noreply.github.com>
2020-11-21 20:52:26 +0900

    Update urlchanger.py

--- urlchanger.py
+++ urlchanger.py
@@ -3,4 +3,9 @@ from urllib.parse import urlparse
 
 url = urlparse(sys.argv[1])
 
-if 
+if argv[2] == 's':
+    if url.scheme == argv[3]:
+        print(url)
+    else:
+        newurl = url.replace(url.scheme, argv[3])
+        print(newurl)
