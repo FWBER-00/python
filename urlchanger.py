01e7d231e8da4dcfa9c7fc8f3c86df97fe22290c
FWBER-00 <74819519+FWBER-00@users.noreply.github.com>
2020-11-23 18:52:31 +0900

    Update urlchanger.py

--- urlchanger.py
+++ urlchanger.py
@@ -3,9 +3,46 @@ from urllib.parse import urlparse
 
 url = urlparse(sys.argv[1])
 
-if argv[2] == 's':
-    if url.scheme == argv[3]:
-        print(url)
+if len(sys.argv) != 4:
+    print("Insufficient arguments")
+
+else:
+    if str(sys.argv[2]) == 's':
+        if url.scheme == str(sys.argv[3]):
+            print(url)
+        else:
+            newurl = url.replace(url.scheme, str(sys.argv[3]))
+            print(newurl)
+ 
+    elif str(sys.argv[2]) == 'n':
+        if url.netloc == str(sys.argv[3]):
+            print(url)
+        else:
+            newurl = url.replace(url.netloc, str(sys.argv[3]))
+            print(newurl)
+
+    elif str(sys.argv[2]) == 'p':
+        if url.path == str(sys.argv[3]):
+            print(url)
+        else:
+            newurl = url.replace(url.path, str(sys.argv[3]))
+            print(newurl)
+
+
+    elif str(sys.argv[2]) == 'q':
+        if url.query == str(sys.argv[3]):
+            print(url)
+        else:
+            newurl = url.replace(url.query, str(sys.argv[3]))
+            print(newurl)
+
+    elif str(sys.argv[2]) == 'f':
+        if url.fragment == str(sys.argv[3]):
+            print(url)
+        else:
+            newurl = url.replace(url.fragment, str(sys.argv[3]))
+            print(newurl)
     else:
-        newurl = url.replace(url.scheme, argv[3])
-        print(newurl)
+        print("Wrong argument")
+
+
